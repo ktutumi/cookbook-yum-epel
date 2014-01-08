dc12bf2b72d0303589c2226c887c195c8a6ccee5
Koichi Tsutsumi <koichi.tsutsumi@gmail.com>
2014-01-08 10:29:56 +0900

    first commit

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name             'yum-epel'
+maintainer       'Koichi Tsutsumi'
+maintainer_email 'Koichi Tsutsumi <koichi.tsutsumi@gmail.com>'
+license          'Apache 2.0'
+description      'Installs/Configures yum-epel'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+depends 'yum', '~> 3.0'
